b3e871587c53b825fda4506bbc6aa576d9147676
SVan22447 <101337701+SVan22447@users.noreply.github.com>
2025-04-09 16:11:39 +0300

    ....

--- index.py
+++ index.py
@@ -1,152 +1,124 @@
-import json
-import sqlite3
-import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, CallbackContext, filters
-from threading import Timer
 
-# Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+import json
+import logging
+from collections import defaultdict
+from typing import Optional
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application,
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ExtBot,
+    TypeHandler,
+)
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 token = config["TOKEN"]
 
-# Создание базы данных и таблиц
-def create_db():
-    conn = sqlite3.connect('reminders.db')
-    c = conn.cursor()
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-    # Создание таблицы команд
-    c.execute('''CREATE TABLE IF NOT EXISTS teams (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT)''')
+logger = logging.getLogger(__name__)
 
-    # Создание таблицы напоминаний
-    c.execute('''CREATE TABLE IF NOT EXISTS reminders (
-                    id INTEGER PRIMARY KEY,
-                    user_id INTEGER,
-                    message TEXT,
-                    time TEXT,
-                    is_group BOOLEAN)''')
 
-    conn.commit()
-    conn.close()
+class ChatData:
+    """Custom class for chat_data. Here we store data per message."""
 
-# Функция проверки напоминаний
-def check_reminders(context: CallbackContext):
-    conn = sqlite3.connect('reminders.db')
-    c = conn.cursor()
-    c.execute("SELECT user_id, message FROM reminders WHERE time <= datetime('now') AND is_group = 0")
-    reminders = c.fetchall()
-    
-    for user_id, message in reminders:
-        context.bot.send_message(chat_id=user_id, text=message)
-        c.execute("DELETE FROM reminders WHERE user_id = ? AND message = ?", (user_id, message))
+    def __init__(self) -> None:
+        self.clicks_per_message: defaultdict[int, int] = defaultdict(int)
 
-    conn.commit()
-    conn.close()
-    Timer(60, check_reminders, [context]).start()  # Проверка каждую минуту
 
-# Начальная команда
-def start(update: Update, context: CallbackContext):
-    keyboard = [
-        [InlineKeyboardButton("Команды", callback_data='commands')],
-        [InlineKeyboardButton("Личные напоминания", callback_data='personal_reminders')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите опцию:', reply_markup=reply_markup)
+# The [ExtBot, dict, ChatData, dict] is for type checkers like mypy
+# class CustomContext(CallbackContext[ExtBot, dict, ChatData, dict]):
+#     """Custom class for context."""
 
-# Обработка нажатий на кнопки
-def button(update: Update, context: CallbackContext):
+#     def __init__(
+#         self,
+#         application: Application,
+#         chat_id: Optional[int] = None,
+#         user_id: Optional[int] = None,
+#     ):
+#         super().__init__(application=application, chat_id=chat_id, user_id=user_id)
+#         self._message_id: Optional[int] = None
+
+#     @property
+#     def bot_user_ids(self) -> set[int]:
+#         """Custom shortcut to access a value stored in the bot_data dict"""
+#         return self.bot_data.setdefault("user_ids", set())
+
+#     @property
+#     def message_clicks(self) -> Optional[int]:
+#         """Access the number of clicks for the message this context object was built for."""
+#         if self._message_id:
+#             return self.chat_data.clicks_per_message[self._message_id]
+#         return None
+
+#     @message_clicks.setter
+#     def message_clicks(self, value: int) -> None:
+#         """Allow to change the count"""
+#         if not self._message_id:
+#             raise RuntimeError("There is no message associated with this context object.")
+#         self.chat_data.clicks_per_message[self._message_id] = value
+
+#     @classmethod
+#     def from_update(cls, update: object, application: "Application") -> "CustomContext":
+#         """Override from_update to set _message_id."""
+#         # Make sure to call super()
+#         context = super().from_update(update, application)
+
+#         if context.chat_data and isinstance(update, Update) and update.effective_message:
+#             # pylint: disable=protected-access
+#             context._message_id = update.effective_message.message_id
+
+#         # Remember to return the object
+#         return context
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Display a message with a button."""
+   
+    keyboard=[
+        InlineKeyboardButton("Команды", callback_data='commands'),
+        InlineKeyboardButton("Личные напоминания", callback_data='personal_reminders')
+    ],
+    reply_markup = InlineKeyboardMarkup(keyboard)   
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
-    query.answer()
 
-    if query.data == 'commands':
-        keyboard = [
-            [InlineKeyboardButton("Создать команду", callback_data='create_team')],
-            [InlineKeyboardButton("Просмотреть команды", callback_data='view_teams')],
-            [InlineKeyboardButton("Назад", callback_data='back')]
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        query.edit_message_text(text="Выберите действие:", reply_markup=reply_markup)
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
 
-    elif query.data == 'personal_reminders':
-        keyboard = [
-            [InlineKeyboardButton("Создать напоминание", callback_data='create_reminder')],
-            [InlineKeyboardButton("Просмотреть напоминания", callback_data='view_reminders')],
-            [InlineKeyboardButton("Назад", callback_data='back')]
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        query.edit_message_text(text="Выберите действие:", reply_markup=reply_markup)
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
-    elif query.data == 'back':
-        start(update, context)
 
-    elif query.data == 'create_team':
-        query.edit_message_text(text="Введите название команды:")
-        return
 
-    elif query.data == 'view_teams':
-        # Логика для просмотра команд
-        pass
+def main() -> None:
+    """Run the bot."""
+    context_types = ContextTypes(context=ContextTypes.DEFAULT_TYPE, chat_data=ChatData)
+    application = Application.builder().token(token).context_types(context_types).build()
 
-    elif query.data == 'create_reminder':
-        query.edit_message_text(text="Введите ваше напоминание:")
-        return
+    # run track_users in its own group to not interfere with the user handlers
+    # application.add_handler(TypeHandler(Update, track_users), group=-1)
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+    # application.add_handler(CallbackQueryHandler(count_click))
+    # application.add_handler(CommandHandler("print_users", print_users))
 
-    elif query.data == 'view_reminders':
-        # Логика для просмотра напоминаний
-        pass
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-# Обработка текстовых сообщений
-def handle_message(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
-    text = update.message.text
 
-    if update.message.reply_to_message:
-
-        if "Введите название команды:" in update.message.reply_to_message.text:
-            # Логика для создания команды
-            create_team(user_id, text)
-            update.message.reply_text(f"Команда '{text}' создана.")
-            start(update, context)
-
-        elif "Введите ваше напоминание:" in update.message.reply_to_message.text:
-            # Логика для создания напоминания
-            create_reminder(user_id, text)
-            update.message.reply_text(f"Напоминание '{text}' создано.")
-            start(update, context)
-
-# Функция для создания команды в базе данных
-def create_team(user_id, team_name):
-    conn = sqlite3.connect('reminders.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO teams (name) VALUES (?)", (team_name,))
-    conn.commit()
-    conn.close()
-
-# Функция для создания личного напоминания в базе данных
-def create_reminder(user_id, reminder_text):
-    conn = sqlite3.connect('reminders.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO reminders (user_id, message, time, is_group) VALUES (?, ?, datetime('now', '+1 minute'), ?)", (user_id, reminder_text, False))
-    conn.commit()
-    conn.close()
-
-def main():
-    create_db()
-    global updater
-    updater = ApplicationBuilder().token(token).build()
-    updater.add_handler(CommandHandler("start", start))
-    updater.add_handler(CallbackQueryHandler(button))
-    updater.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    
-    # Запуск проверки напоминаний
-    check_reminders(None)
-
-    updater.run_polling()
-    updater.idle()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
